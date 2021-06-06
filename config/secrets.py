5f677b636620d75acce6dc47a7a055e9d0bc708a
galaxar-mafdet <85461662+galaxar-mafdet@users.noreply.github.com>
2021-06-06 17:25:56 -0500

    Update secrets.py

--- config/secrets.py
+++ config/secrets.py
@@ -1,17 +1,5 @@
 class Secrets:
     def __init__(self):
-        # from pollmaster  self.dbl_token = ''  # DBL token (only needed for public bot)
-
-        # try to use mongo db in the cloud
-        # copied these lines from mongo webpage
-        # client = pymongo.MongoClient("mongodb+srv://<username>:<password>@mpl-mongo-cluster-y8c6g.mongodb.net/test?retryWrites=true&w=majority")
-        # db = client.test
-        #
-        # use this instead 
-        # self.mongo_db = "mongodb+srv://pollmaster_mongodb:Pufcib-ricben-boxpe6@mpl-mongo-cluster-y8c6g.mongodb.net/test?retryWrites=true&w=majority"
-
-        # lines form original pollmaster
-        # self.mongo_db = 'mongodb://localhost:27017/pollmaster'
 
         # Oops - don't put the bot token in GitHub!  Discord found it when uploaded and reset the token.
         self.bot_token = 'bot_token_goes_here' # mafdet token
